919e0ea5ec2b53bfbf7f67878f487bed978a0894
Rony cb <mylovepre16@gmail.com>
2020-08-07 15:30:40 -0500

    update serve to strict mode

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,21 @@
 /** @format */
 
 // Register module/require aliases
-import 'module-alias/register';
-
-// Patches
-import { inject, errorHandler } from 'express-custom-error';
-inject(); // Patch express in order to use async / await syntax
-
-// // Require Dependencies
-// import { Server } from 'http';
-import env from 'mandatoryenv';
-import express from 'express';
-import morgan from 'morgan';
+import "module-alias/register";
+//[*] import variables de entorno
+import * as dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
 // Getting base GraphQL Schema
 
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import helmet from 'helmet';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
 
-import logger from '@util/logger';
+import logger from "@util/logger";
 
-// Load .env Enviroment Variables to process.env
-
-env.load(['DB_URL', 'PORT', 'SECRET']);
+//[*] load port from env
+dotenv.config();
 
 const { PORT } = process.env;
 
@@ -31,46 +24,44 @@ const { PORT } = process.env;
 const app = express();
 
 // Configure Express App Instance
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Configure custom logger middleware
 app.use(logger.dev, logger.combined);
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
 
 // This middleware adds the json header to every response
-app.use('*', (req, res, next) => {
-	res.setHeader('Content-Type', 'application/json');
-	next();
+app.use("*", (_req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
 });
 
 // Assign Routes
-import router from './routes/router';
-app.use('/api/rest', router);
-
-// Handle errors
-app.use(errorHandler());
+import router from "./routes/router";
+app.use("/api/rest", router);
 
 // Handle not valid route
-app.use('*', (req, res) => {
-	res.status(404).json({ status: false, message: 'Endpoint Not Found' });
+app.use("*", (_req, res) => {
+  res.status(404).json({ status: false, message: "Endpoint Not Found" });
 });
 
 // Open Server on configurated Port
 const server = app;
 
-server.listen(PORT, () => console.info('Server listening on port ', PORT));
+server.listen(PORT, () => console.info("Server listening on port ", PORT));
 
-let io = require('socket.io')(server);
+//[*] ejemplo socket io
+// let io = require("socket.io")(server);
 
-io.on('connection', socket => {
-	console.log('new client connectes');
-	socket.emit('FromAPI', { info: 'wordkhjhjhhgfg' });
-	socket.on('disconnect', function() {
-		console.log('Cliente desconectado');
-	});
-});
+// io.on("connection", (socket) => {
+//   console.log("new client connectes");
+//   socket.emit("FromAPI", { info: "wordkhjhjhhgfg" });
+//   socket.on("disconnect", function () {
+//     console.log("Cliente desconectado");
+//   });
+// });
