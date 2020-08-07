fb6c6489e1ad8de9e5e9aec2de9b23bb8eedce24
Rony cb <mylovepre16@gmail.com>
2020-08-07 15:32:42 -0500

    updating logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,18 +4,19 @@ import path from "path";
 import fs from "fs";
 
 // log directory path
-const logDirectory = path.resolve(__dirname, '../../log');
+const logDirectory = path.resolve(__dirname, "../../log");
 
 // ensure log directory exists
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 // create a rotating write stream
-const accessLogStream = rfs('access.log', {
-    interval: '1d',
-    path: logDirectory
-})
+const accessLogStream = rfs.createStream("access.log", {
+  size: "10M",
+  interval: "1d",
+  path: logDirectory,
+});
 
 export default {
-    dev: morgan('dev'),
-    combined: morgan('combined', { stream: accessLogStream })
-}
+  dev: morgan("dev"),
+  combined: morgan("combined", { stream: accessLogStream }),
+};
