512c124308ab37eb381bd68a173cd19a06c6c5e3
Rony cb <mylovepre16@gmail.com>
2020-08-07 15:31:49 -0500

    update example controlers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,64 +1,22 @@
 /** @format */
 
-import status from 'http-status';
-import usersModel from '@models/users';
-import has from 'has-keys';
+import status from "http-status";
+import usersModel from "@models/users";
+import has from "@util/hasKeys";
 
 export default {
-	async getCalzadosAll(req, res) {
-		let prueba = usersModel.getCalzado();
-		let observer = prueba.onSnapshot(querySnapshot => {
-			querySnapshot.docChanges().forEach(change => {
-				if (change.type === 'added') {
-					console.log('added: ', change.doc.data());
-					res.write(JSON.stringify(change.doc.data()));
-				}
-				if (change.type === 'modified') {
-					console.log('Modified city: ', change.doc.data());
-					res.write(JSON.stringify(change.doc.data()));
-				}
-				if (change.type === 'removed') {
-					console.log('Removed city: ', change.doc.data());
-					res.write(JSON.stringify(change.doc.data()));
-				}
-				// if (change.type === 'modified') {
-				// 	console.log('Modified city: ', change.doc.data());
-				// }
-				// if (change.type === 'removed') {
-				// 	console.log('Removed city: ', change.doc.data());
-				// }
-			});
+  async getCalzadosAll(_req: any, res: any) {
+    let prueba = usersModel.getCalzado();
+    const resposne = await prueba.doc("/example").get();
+    if (resposne.exists) res.json(resposne.data());
+  },
 
-			// done();
-			// console.log(ele.size);
-			// res.json({ status: false, data: ele.size });
-		});
-
-		// if (data.empty) {
-		// 	res.json({ status: false, message: 'Calzados not found' });
-		// } else {
-		// 	let calzados = [];
-		// 	data.forEach(ele => {
-		// 		calzados.push(ele.data());
-		// 	});
-		// 	res.json({
-		// 		status: true,
-		// 		message: 'Returning calzado',
-		// 		calzados: calzados
-		// 	});
-		// }
-	},
-
-	async getUserById(req, res) {
-		if (!has(req.params, 'id'))
-			throw { code: status.BAD_REQUEST, message: 'You must specify the id' };
-
-		let { id } = req.params;
-
-		let data = null;
-
-		if (!data) throw { code: status.BAD_REQUEST, message: 'User not found' };
-
-		res.json({ status: true, message: 'Returning user', data });
-	}
+  async getUserById(req: any, res: any) {
+    if (!has.getHaskey(req.params, "id"))
+      throw { code: status.BAD_REQUEST, message: "You must specify the id" };
+    let { id } = req.params;
+    let data = null;
+    if (!data) throw { code: status.BAD_REQUEST, message: "User not found" };
+    res.json({ status: true, message: "Returning user", data });
+  },
 };
