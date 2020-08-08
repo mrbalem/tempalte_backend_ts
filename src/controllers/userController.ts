7338e55b2c69ea7d93587c2efe5f16ba6fac6b0b
Rony cb <mylovepre16@gmail.com>
2020-08-07 19:44:57 -0500

    change controller to oop

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,21 +2,34 @@
 
 import status from "http-status";
 import usersModel from "@models/users";
+import { Request, Response } from "express";
 import has from "@util/hasKeys";
 
-export default {
-  async getCalzadosAll(_req: any, res: any) {
-    let prueba = usersModel.getCalzado();
-    const resposne = await prueba.doc("/example").get();
-    if (resposne.exists) res.json(resposne.data());
-  },
+/**
+ * @author DigitalNew
+ * @see https://digitalnew.solutions/
+ * @version 1.0.0
+ * @template UserControllers
+ */
 
-  async getUserById(req: any, res: any) {
+export default class UserControllers {
+  async getCalzadosAll(_req: Request, res: Response) {
+    let prueba = usersModel.getCalzado();
+    try {
+      const resposne = await prueba.doc("example").get();
+      if (resposne.exists) res.json(resposne.data());
+      res.json({ status: false, message: "not found user" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json("opps. Ocurrio un error inesperado");
+    }
+  }
+  async getUserById(req: Request, res: Response) {
     if (!has.getHaskey(req.params, "id"))
       throw { code: status.BAD_REQUEST, message: "You must specify the id" };
     let { id } = req.params;
     let data = null;
     if (!data) throw { code: status.BAD_REQUEST, message: "User not found" };
     res.json({ status: true, message: "Returning user", data });
-  },
-};
+  }
+}
