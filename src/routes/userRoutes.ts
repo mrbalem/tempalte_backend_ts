b9ef11bf7aff11c07a43ba243029b75a45f1eac8
Rony cb <mylovepre16@gmail.com>
2020-08-07 19:45:31 -0500

    change user Router oop

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,28 @@
 /** @format */
+import express from "express";
 
-import express from 'express';
+// [*] controlador para resolver las consultas.
+import usersController from "@controllers/userController";
 
-const router = express.Router();
-import usersController from '@controllers/userController';
-
-router.get('/users/', usersController.getCalzadosAll);
-
-export default router;
+/**
+ * @author DigitalNew
+ * @see https://digitalnew.solutions/
+ * @see https://github.com/DigitalNews/
+ * @version 1.0.0
+ * @process 2
+ */
+export default class UserRouter {
+  public router: express.Application;
+  //[*] intanciasmos un nuevo objeto del controlador user
+  public userController: usersController = new usersController();
+  constructor(router: express.Application) {
+    this.router = router;
+  }
+  /**
+   * @function Endpoint
+   * @param path endoinpoint base
+   */
+  public Endpoint(path: string): void {
+    this.router.get(path + "/users", this.userController.getCalzadosAll);
+  }
+}
